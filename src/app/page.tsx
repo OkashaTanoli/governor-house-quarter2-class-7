717d792dfa93f18a0feec117ea18a7dcfb3748ca
OkashaTanoli <okashatanoli12345@gmail.com>
2024-10-26 21:52:29 +0500

    flex grid class

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,43 @@
-import Image from "next/image";
+import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    // flex box : direction row
+    // <div className="h-screen bg-purple-500 flex gap-5 justify-start items-end">
+    //   <div className="bg-red-500 w-[100px]">BOX 1</div>
+    //   <div className="bg-green-500 w-[100px]">BOX 2</div>
+    //   <div className="bg-slate-500 w-[100px]">BOX 3</div>
+    //   <div className="bg-orange-500 w-[100px]">
+    //     Lorem ipsum dolor sit amet consectetur adipisicing elit. Error quisquam necessitatibus iure
+    //   </div>
+    // </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    // flex box direction column 
+
+    // <div className="bg-[#044E83] flex justify-between items-center px-10">
+    //   <Image src={'/logo.png'} width={75} height={75} alt='Logo' />
+    //   <h3 className='text-2xl font-bold text-white'>Tuition Free Education Program on Latest Technologies</h3>
+    //   <nav className='text-white flex gap-5'>
+    //     <div>Home</div>
+    //     <div>About</div>
+    //     <div>Contact</div>
+    //     <div>Apply</div>
+    //     <div>Courses</div>
+    //   </nav>
+    // </div>
+
+
+    // <div className='w-[500px] h-[500px] bg-red-300 relative'>
+    //   <div className='w-[100px] h-[100px] bg-gray-600 absolute bottom-5 right-5'></div>
+    //   <div className='w-[100px] h-[100px] bg-green-600'></div>
+    // </div>
+
+    <div className='grid grid-cols-[auto,1fr,1fr,1fr] gap-5'>
+      <div className="bg-red-500">BOX 1</div>
+      <div className="bg-green-500">BOX 2</div>
+      <div className="bg-slate-500">BOX 3</div>
+      <div className="bg-black">BOX 4</div>
+      <div className="bg-orange-400">BOX 5</div>
     </div>
   );
 }
